88a753723f75d79e7b075a81bab380e39d4b428e
Jim Brown <jb.github@rcairgallery.com>
2015-10-31 14:03:05 -0400

    Update theme_description.php

--- zenfluid/theme_description.php
+++ zenfluid/theme_description.php
@@ -1,10 +1,10 @@
 <?php
-// force UTF-8 Ø
+// force UTF-8 Ã˜
 
 // Zenphoto theme definition file
 $theme_description['name'] = 'ZenFluid';
 $theme_description['author'] = 'Jim Brown';
 $theme_description['version'] = '1.0';
 $theme_description['date'] = '2015/04/27';
-$theme_description['desc'] = "A fluid theme based on the original ZenPage theme.";
+$theme_description['desc'] = "A fluid theme for Zenphoto20 based on the original ZenPage theme.";
 ?>
