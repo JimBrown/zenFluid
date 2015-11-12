db7bfee10c31b0ec5448597a41b20c03666eba81
Jim Brown <jb.github@rcairgallery.com>
2015-11-12 11:53:37 -0500

    Tweak album page layout.

--- zenfluid/album.php
+++ zenfluid/album.php
@@ -17,73 +17,75 @@
 				<?php $doSlideShowLink = false;
 				$stageWidth = getOption('zenfluid_stagewidth');
 				$stageStyle = ($stageWidth > 0) ? 'style="max-width: ' . $stageWidth . 'px; margin-left: auto; margin-right: auto;"' : ''; ?>
-				<div id="stage" <?php echo $stageStyle;?>>
 				<?php if (getOption('zenfluid_titletop')) { ?>
-					<div id="title" class="border colour">
-						<?php if (getOption('zenfluid_titlebreadcrumb')) { ?>
-							<a href="<?php echo getGalleryIndexURL(); ?>" title="<?php gettext('Home'); ?>"><?php echo gettext("Home"); ?></a> : <?php
-							printParentBreadcrumb("", " : ", " : ");
-						} ?>
-						<strong><?php printAlbumTitle(); ?></strong>
+					<div id="stage" <?php echo $stageStyle;?>>
+						<div id="title" class="border colour">
+							<?php if (getOption('zenfluid_titlebreadcrumb')) { ?>
+								<a href="<?php echo getGalleryIndexURL(); ?>" title="<?php gettext('Home'); ?>"><?php echo gettext("Home"); ?></a> : <?php
+								printParentBreadcrumb("", " : ", " : ");
+							} ?>
+							<strong><?php printAlbumTitle(); ?></strong>
+						</div>
 					</div>
 				<?php } ?>
-				</div>
-					<div id="albums">
-						<?php while (next_album()):?>
-							<div id="thumbs" class="border colour">
-								<a href="<?php echo html_encode(getAlbumURL());?>" title="<?php echo gettext('View: '); printBareAlbumTitle();?>">
-									<div id="thumbimage">
-										<?php printAlbumThumbImage(getBareAlbumTitle(),"border"); ?>
-										<div id="thumbtitle">
-											<?php $numItems = getNumImages() + getNumAlbums();
-											printAlbumTitle(); echo ' (' . $numItems . ')';
-											echo "<p>" . shortenContent(strip_tags(getAlbumDesc()), 150, getOption("zenpage_textshorten_indicator")) . "</p>"; ?>
-										</div>
+				<div id="albums">
+					<?php while (next_album()):?>
+						<div id="thumbs" class="border colour">
+							<a href="<?php echo html_encode(getAlbumURL());?>" title="<?php echo gettext('View: '); printBareAlbumTitle();?>">
+								<div id="thumbimage">
+									<?php printAlbumThumbImage(getBareAlbumTitle(),"border"); ?>
+									<div id="thumbtitle">
+										<?php $numItems = getNumImages() + getNumAlbums();
+										printAlbumTitle(); echo ' (' . $numItems . ')';
+										echo "<p>" . shortenContent(strip_tags(getAlbumDesc()), 150, getOption("zenpage_textshorten_indicator")) . "</p>"; ?>
 									</div>
-								</a>
-							</div>
-						<?php endwhile;
-						if (getOption('zenfluid_transitionnewrow')) { ?>
-							<div class="clearing"></div>
-						<?php }
-						while (next_image()):
-							if (isImagePhoto()) {
-								$doSlideShowLink = true;
-							} ?>
-							<div id="thumbs" class="border colour">
-								<a href="<?php echo html_encode(getImageURL());?>" title="<?php echo gettext('View: '); printBareImageTitle();?>">
-									<div id="thumbimage">
-										<?php printImageThumb(getBareImageTitle(),"border");
-										if (isImageVideo()) { ?>
-											<img id="videoplay" src="<?php echo $_zp_themeroot; ?>/images/videoplay.png">
-										<?php } ?>
-										<div id="thumbtitle">
-											<?php printImageTitle();
-											echo "<p>" . shortenContent(strip_tags(getImageDesc()), 150, getOption("zenpage_textshorten_indicator")) . "</p>"; ?>
-										</div>
-									</div>
-								</a>
-							</div>
-						<?php endwhile; ?>
-					</div>
-					<div class="clearing"></div>
-				<div id="stage" <?php echo $stageStyle;?>>
-					<div id="albumbuttons">
-						<?php if (hasPrevPage() || hasNextPage()) { ?>
-							<div id="button" class="border colour">
-								<?php printPageListWithNav("Prev ", " Next", false, true, 'taglist', NULL, true); ?>
-							</div>
-						<?php }
-						if ($doSlideShowLink && function_exists('printSlideShowLink')) { ?>
-							<div id="button" class="border colour">
-								<div id="slideshowlink" style="display: inline-block; float: none;">
-									<?php printSlideShowLink();?>
 								</div>
-							</div>
-						<?php } ?>
-					</div>
-					<div class="clearing"></div>
-					<?php if (getOption('zenfluid_titletop')) {
+							</a>
+						</div>
+					<?php endwhile;
+					if (getOption('zenfluid_transitionnewrow')) { ?>
+						<div class="clearing"></div>
+					<?php }
+					while (next_image()):
+						if (isImagePhoto()) {
+							$doSlideShowLink = true;
+						} ?>
+						<div id="thumbs" class="border colour">
+							<a href="<?php echo html_encode(getImageURL());?>" title="<?php echo gettext('View: '); printBareImageTitle();?>">
+								<div id="thumbimage">
+									<?php printImageThumb(getBareImageTitle(),"border");
+									if (isImageVideo()) { ?>
+										<img id="videoplay" src="<?php echo $_zp_themeroot; ?>/images/videoplay.png">
+									<?php } ?>
+									<div id="thumbtitle">
+										<?php printImageTitle();
+										echo "<p>" . shortenContent(strip_tags(getImageDesc()), 150, getOption("zenpage_textshorten_indicator")) . "</p>"; ?>
+									</div>
+								</div>
+							</a>
+						</div>
+					<?php endwhile; ?>
+				</div>
+				<div class="clearing"></div>
+				<div id="stage" <?php echo $stageStyle;?>>
+					<?php if (hasPrevPage() || hasNextPage() || ($doSlideShowLink && function_exists('printSlideShowLink'))) { ?>
+						<div id="albumbuttons">
+							<?php if (hasPrevPage() || hasNextPage()) { ?>
+								<div id="button" class="border colour">
+									<?php printPageListWithNav("Prev ", " Next", false, true, 'taglist', NULL, true); ?>
+								</div>
+							<?php }
+							if ($doSlideShowLink && function_exists('printSlideShowLink')) { ?>
+								<div id="button" class="border colour">
+									<div id="slideshowlink" style="display: inline-block; float: none;">
+										<?php printSlideShowLink();?>
+									</div>
+								</div>
+							<?php } ?>
+						</div>
+						<div class="clearing"></div>
+					<?php }
+					if (getOption('zenfluid_titletop')) {
 						if (getAlbumDesc()) { ?>
 							<div id="title" class="border colour">
 								<?php printAlbumDesc(); ?>
